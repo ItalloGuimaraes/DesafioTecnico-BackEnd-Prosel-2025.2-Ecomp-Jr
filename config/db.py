303eb1187429c5ef0431e33294de99944288a607
Ítallo Guimarães <italloguimaraes1@gmail.com>
2025-10-12 14:01:29 -0300

    Update: correção dos comentários

--- config/db.py
+++ config/db.py
@@ -1,25 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# --- Defina suas credenciais aqui ---
+
 username = "postgres"
 password = "itallo1230"
 host = "localhost"
 port = 5432
 database = "ecomp_jr_api"
 
-# --- Crie la string de conexão (URL DSN) manualmente ---
 DATABASE_URL = f"postgresql://{username}:{password}@{host}:{port}/{database}"
 
-# --- Crie o engine usando a string de conexão ---
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
 
-# --- ATUALIZAÇÃO: Adicionar a função get_db ---
+
 def get_db():
-    """
-    Função 'dependency' que cria e fornece uma sessão de banco de dados por requisição.
-    """
     db = Session()
     try:
         yield db
--- model/admins.py
+++ model/admins.py
@@ -1,10 +1,9 @@
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base # Importando o Base de um modelo já existente
+from sqlalchemy.orm import declarative_base 
 from config.db import engine
 
 Base = declarative_base()
 
-# Verifique se o nome da classe é exatamente "Admin"
 class Admin(Base):
     __tablename__ = "admins"
     
@@ -12,5 +11,5 @@ class Admin(Base):
     username = Column(String(255), unique=True, index=True)
     hashed_password = Column(String(255))
 
-# Garante que a tabela seja criada no banco, se não existir
+
 Base.metadata.create_all(bind=engine)
